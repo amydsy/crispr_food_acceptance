c252f1133143edd6c9589201a088245a3d347857
邓舒月 <dengshuyue@MacBook-Air-2.local>
2022-10-22 22:54:06 -0400

    source code

---
+++ cleaning_code/data_cleaning_apple copy.R
@@ -0,0 +1,192 @@
+# CRISPR Conjoint Analysis 
+setwd("~/Desktop//crispr_r")
+
+# install packages for conjoint 
+# install.packages(c("cluster", "survival"))
+# install.packages("conjoint")
+# install.packages("tidyverse")
+# install.packages("radiant")
+# install.packages('gtools')
+library(tidyverse)
+library(tidyselect)
+library(conjoint)
+library(readxl)
+library(radiant)
+library(data.table)
+library(tidyr)
+library('fastDummies')
+library(gtools)
+
+
+################################################################################
+# Cleaning the data 
+
+dir <- list()
+#dir$root <- dirname(getwd())
+dir$root <- getwd()
+#dir$source   <- paste(dir$root,"/AT_raw.csv",sep="")
+dir$output   <- paste(dir$root,"/data_clean",sep="")
+
+
+df <- read_csv("AT_raw.csv") # get data from csv 
+exp_design <- read.csv(file = "AppleTomato_tomato1_Design.csv", header = TRUE, stringsAsFactors = FALSE)
+
+# explain variable in design_exp: 
+#Concept is the choice 4 profile, none option is not included 
+
+df <-rename(df, Version = sys_CBCVersion_tomato1)
+df <-rename(df, id = sys_SequentialRespNum)
+
+
+# drop unrelated variables, now only keep apple
+
+#df <- df[-c(2:21,28:33)]
+#df <- df[-c(1:5,7:31),]
+
+# reshape data to wide format one id with 6 task
+
+df <- pivot_longer(df,
+                   cols = starts_with("apple_Random"),
+                   names_to = "task",
+                   names_prefix = "apple_Random",
+                   values_to = "choice",
+                   values_drop_na = TRUE
+)
+# cast task from character to numeric
+df$task <- as.numeric(df$task)
+
+# head(select(df, id, task),4)
+
+############ Change to standard wide ############
+
+N = 1
+#Every N rows after which empty rows should be inserted
+after_rows = 4
+exp_design <- do.call(rbind, lapply(split(exp_design, ceiling(1:NROW(exp_design)/after_rows)),
+                                    function(a) rbind(a, replace(a[1:N,], TRUE, ""))))
+
+insert <- matrix(5,1800)
+exp_design$Concept[c(FALSE, FALSE, FALSE, FALSE, TRUE)] <- insert[, 1]
+
+for (row in 2:length(exp_design$Task)){ # 2 so you don't affect column names
+  if(exp_design$Task[row] == "") {    # if its empty...
+    exp_design$Task[row] = exp_design$Task[row-1] # ...replace with previous row's value
+  }
+}
+
+for (row in 2:length(exp_design$Version)){ # 2 so you don't affect column names
+  if(exp_design$Version[row] == "") {    # if its empty...
+    exp_design$Version[row] = exp_design$Version[row-1] # ...replace with previous row's value
+  }
+}
+
+
+# merge 
+
+####### ?? why ?? why id 6 is behind ??????????????????
+
+df$id[df$id==6] <- 06
+
+df_merge = merge(df, exp_design, by.x=c('id','task', 'choice'), by.y=c('Version','Task','Concept'),sort = T)
+df_merge2<- df_merge[ ,c(1:3,62:65)]
+
+###### create Dummy for each level of attributes for each profile  
+# First change the exp_design_ori from long to wide  
+exp_design_ori <- read.csv(file = "AppleTomato_tomato1_Design.csv", header = TRUE, stringsAsFactors = FALSE)
+
+exp_wide <- exp_design_ori %>% 
+  pivot_wider(names_from = Concept, 
+              values_from = c("Att.1...Production.Method", "Att.3...Purchase.Method", "Att.2...Price"))
+
+# merge again  
+df_merge3 = merge(df_merge, exp_wide, by.x=c('id','task'), by.y=c('Version','Task'))
+df_merge4<- df_merge3[ ,c(1:3,62,66:77)]
+
+# create dummy 
+#install.packages('fastDummies')
+
+df_merge4 <- df_merge4 %>% 
+  rename(treatment = sys_block_set_1)
+
+df_merge5 <- dummy_cols(df_merge4,
+                        select_columns = c( 'Att.1...Production.Method_1', 
+                                            'Att.1...Production.Method_2',
+                                            'Att.1...Production.Method_3',
+                                            'Att.1...Production.Method_4',
+                                            'Att.1...Production.Method_5',
+                                            'treatment',
+                                            
+                                            'Att.3...Purchase.Method_1',
+                                            'Att.3...Purchase.Method_2',
+                                            'Att.3...Purchase.Method_3',
+                                            'Att.3...Purchase.Method_4',
+                                            'Att.3...Purchase.Method_5'
+                                            ))
+
+#'Att.2...Price_1','Att.2...Price_2',
+#'Att.2...Price_3', 'Att.2...Price_4',
+
+
+
+
+# rename for profile 1 
+df_merge6 <- df_merge5 %>% 
+  rename(
+    organic_1 = Att.1...Production.Method_1_1, nongmo_1 = Att.1...Production.Method_1_2,
+    conventional_1 = Att.1...Production.Method_1_3, CRISPR_1 = Att.1...Production.Method_1_4,
+    GMO_1 = Att.1...Production.Method_1_5, price_1 = Att.2...Price_1, 
+    instore_1 = Att.3...Purchase.Method_1_1, online_1 = Att.3...Purchase.Method_1_2,
+  )
+
+# rename for profile 2
+
+df_merge6 <- df_merge6 %>% 
+  rename(
+    organic_2 = Att.1...Production.Method_2_1, nongmo_2 = Att.1...Production.Method_2_2,
+    conventional_2 = Att.1...Production.Method_2_3, CRISPR_2 = Att.1...Production.Method_2_4,
+    GMO_2 = Att.1...Production.Method_2_5, price_2 = Att.2...Price_2, 
+    instore_2 = Att.3...Purchase.Method_2_1, online_2 = Att.3...Purchase.Method_2_2,
+  )
+
+# rename profile 3&4
+df_merge7 <- df_merge6 %>% 
+  rename(
+    organic_3 = Att.1...Production.Method_3_1, nongmo_3 = Att.1...Production.Method_3_2,
+    conventional_3 = Att.1...Production.Method_3_3, CRISPR_3 = Att.1...Production.Method_3_4,
+    GMO_3 = Att.1...Production.Method_3_5, price_3 = Att.2...Price_3, 
+    instore_3 = Att.3...Purchase.Method_3_1, online_3 = Att.3...Purchase.Method_3_2,
+    
+    organic_4 = Att.1...Production.Method_4_1, nongmo_4 = Att.1...Production.Method_4_2,
+    conventional_4 = Att.1...Production.Method_4_3, CRISPR_4 = Att.1...Production.Method_4_4,
+    GMO_4 = Att.1...Production.Method_4_5, price_4 = Att.2...Price_4, 
+    instore_4 = Att.3...Purchase.Method_4_1, online_4 = Att.3...Purchase.Method_4_2,
+  )
+
+drop <- c( 'Att.1...Production.Method_1', 'Att.1...Production.Method_2',
+           'Att.1...Production.Method_3', 'Att.1...Production.Method_4',
+           'Att.3...Purchase.Method_1',  'Att.3...Purchase.Method_2',
+           'Att.3...Purchase.Method_3', 'Att.3...Purchase.Method_4',
+           'Att.2...Price_1','Att.2...Price_2',
+           'Att.2...Price_3', 'Att.2...Price_4',
+           'instore_1','instore_2','instore_3','instore_4','task'
+           ) 
+
+# 'GMO_1','GMO_2','GMO_3','GMO_4'
+
+df_clean3 = df_merge7[,!(names(df_merge7) %in% drop)]
+
+
+
+######select participants
+df_apple<- df_clean3[df_clean3$id >= 1, ]
+
+
+# export clean data in data_clean
+OutPath<- "~/Desktop/crispr_r/data_clean/df_apple.csv"
+
+OutTbl <- df_apple
+write.csv(OutTbl, file = OutPath)
+
+#end 
+
+
---
+++ hb_analysis.R
@@ -0,0 +1,212 @@
+
+#-----------------------------prepare data--------------------------------------
+#install.packages("RSGHB")
+#install.packages("dplyr")
+#install.packages("CRAN")
+library(readr)
+library(RSGHB)
+library(dplyr)
+library(tidyfst)
+library(glue)
+library(stringr) 
+
+# set working directory 
+# repo root
+repo_root <- getwd()
+output_folder <- "output/"
+
+# get cleaned data 
+df <- read.csv(list.files("data_clean/apple/", full.names = T))
+# manipulate data to feed the HB model
+set_id = rep(1:6, times = nrow(df)/6)
+df$S_id <- set_id
+df = subset(df, select = -c(1))
+
+# rename columns
+colnames(df)[1] <- 'ID'
+colnames(df)[2] <- 'Choice'
+
+#----------------------------select treatment-----------------------------------
+df<-df[!(df$ID==16 ),]  
+df<-df[(df$treatment_1==1 ),]  
+
+#-----------------------set GMO and in-store as -1------------------------------
+
+x_1 <- c("organic_1", "nongmo_1", "CRISPR_1", "conventional_1")
+x_2 <- c("organic_2", "nongmo_2", "CRISPR_2", "conventional_2")
+x_3 <- c("organic_3", "nongmo_3", "CRISPR_3", "conventional_3")
+x_4 <- c("organic_4", "nongmo_4", "CRISPR_4", "conventional_4")
+
+df2 <- df %>% mutate_at(.vars = x_1, funs(ifelse(GMO_1 == 1, -1, .)))
+df3 <- df2 %>% mutate_at(.vars = x_2, funs(ifelse(GMO_2 == 1, -1, .)))
+df4 <- df3 %>% mutate_at(.vars = x_3, funs(ifelse(GMO_3 == 1, -1, .)))
+df5 <- df4 %>% mutate_at(.vars = x_4, funs(ifelse(GMO_4 == 1, -1, .)))
+
+x=1:4
+online_all = sapply(x, function(xi) {glue("online_{xi}")}) 
+df5[, online_all][df5[, online_all] == 0] <- -1
+
+
+
+df <- df5 #[(df5$treatment_1==1),] 
+
+
+#--------------------------start analysis---------------------------------------
+
+# SETTINGS for RSGHB 
+modelname <- "hb_likelihood"       # used for output
+gNCREP <- 10000    # Number of burn-in iterations 
+gNEREP <- 1000  	# Number of iterations to keep for averaging after convergence has been reached
+gNSKIP <- 5  	# Number of iterations to do in between retaining draws for averaging
+gINFOSKIP <- 250    	# How frequently to print info about the iteration process
+degreesOfFreedom <- 5 # Additional degrees of freedom for the prior covariance matrix (not including the number of parameters. (Defaults to 5)
+#priorVariance <- c(1,1,1,1,1,1, 0.5) # last param is for price (Defaults to 2.0)
+priorVariance <- 1
+writeModel <- T # Write model files?
+gStoreDraws <- TRUE # Store random draws?
+
+# The choice vectors: Dummy coding the choice vector allows for 
+# easier coding of the the likelihood calculations.
+df$choice1  <- (df$Choice==1)
+df$choice2  <- (df$Choice==2)
+df$choice3  <- (df$Choice==3)
+df$choice4  <- (df$Choice==4)
+df$choice5  <- (df$Choice==5)
+
+
+#df <- df %>%
+#  mutate(price_1 = recode(price_1, "1"= 1.99 , "2" = 2.99, "3" = 3.99, "4" = 4.99) 
+#  )
+#df <- df %>%
+#  mutate(price_2 = recode(price_2, "1"= 1.99 , "2" = 2.99, "3" = 3.99, "4" = 4.99) 
+#  )
+#df <- df %>%
+#  mutate(price_3 = recode(price_3, "1"= 1.99 , "2" = 2.99, "3" = 3.99, "4" = 4.99) 
+#  )
+#df <- df %>%
+#  mutate(price_4 = recode(price_4, "1"= 1.99 , "2" = 2.99, "3" = 3.99, "4" = 4.99) 
+#  )
+
+
+# Fixed effects for images and incentive alignment (ia) conditions and for validation choices in these conditions
+#gVarNamesFixed <- c( "btreatment_1" ,"btreatment_2")
+# random effects
+gVarNamesNormal <- c("none", "organic", "nongmo", "CRISPR", "conventional",'online' ,"price")
+
+# Assuming normal distributions for all parameters, except price, which is one-sided triangular distribution
+gDIST <- c(rep(1,length(gVarNamesNormal)-1), 4)	
+
+# STARTING VALUES
+#FC <- c(rep(0, length(gVarNamesFixed))) # for the fixed coefficients
+svN <- c(rep(1, length(gVarNamesNormal) - 1), 1)  # for the random coefficients
+
+# CONTROL LIST TO PASS TO doHB
+control <- list(
+  modelname=modelname,
+  #gVarNamesFixed = gVarNamesFixed,
+ # FC = FC,
+  gVarNamesNormal=gVarNamesNormal,
+  gDIST=gDIST,
+#  svN=svN,
+  gNCREP=gNCREP,
+  gNEREP=gNEREP,
+  gNSKIP=gNSKIP,
+  gINFOSKIP=gINFOSKIP,
+  gStoreDraws=gStoreDraws,  
+  writeModel= writeModel,
+  degreesOfFreedom=degreesOfFreedom,
+  priorVariance=priorVariance
+#  constraintsNorm=constraintsNorm
+)
+
+
+# likelihood function
+
+likelihood<- function(fc ,b)
+{
+  # fixed effects for scale adjustment parameters
+# cc <- 1
+ # btreatment_1  <- fc[cc]; cc <- cc + 1
+ # btreatment_2  <- fc[cc]; cc <- cc + 1
+
+  # random effects
+  cc = 1 # internal pointer
+  bnone <- b[,cc];cc=cc+1
+  borganic  <- b[,cc];cc=cc+1
+  bnongmo <- b[,cc];cc=cc+1 
+  bCRISPR <- b[,cc];cc=cc+1 
+  bconventional <- b[,cc];cc=cc+1 
+  bonline  <- b[,cc];cc=cc+1 
+  bprice  <- b[cc]; cc <- cc + 1
+  # in this stylized model bprice is normalized to 1.
+  # for the Allenby et al. (2014) model, set bgamma to 1 and estimate a bprice parameter instead (adjust the v_i and h_i formulations below accordingly).
+  # for  the Sonnier, Allenby, and Otter (2007) specification estimate bgamma as 1/b[,cc]
+  
+  
+  
+  # scale adjustment factors for the different experimental conditions
+  #treatment_estimation <- exp(btreatment_1 * df$treatment_1 + btreatment_2 * df$treatment_2 )
+  
+  # utility functions for the regular choice sets
+  # Utility functions 
+  # "none", "organic", "nongmo", "CRISPR", "conventional",'online' ,"gamma" 
+ 
+  v1 <- (borganic * df$organic_1 + bnongmo * df$nongmo_1 + bCRISPR * df$CRISPR_1 
+             + bconventional * df$conventional_1 + bonline * df$online_1 - bprice * df$price_1)
+          
+  v2 <- (borganic * df$organic_2 + bnongmo * df$nongmo_2 + bCRISPR * df$CRISPR_2 
+             + bconventional * df$conventional_2 + bonline * df$online_2 - bprice * df$price_2)
+  v3 <- (borganic * df$organic_3 + bnongmo * df$nongmo_3 + bCRISPR * df$CRISPR_3 
+             + bconventional * df$conventional_3 + bonline * df$online_3 - bprice * df$price_3)
+  v4 <- (borganic * df$organic_4 + bnongmo * df$nongmo_4 + bCRISPR * df$CRISPR_4 
+             + bconventional * df$conventional_4 + bonline * df$online_4 - bprice * df$price_4)
+  
+  v5 <- bprice * bnone
+
+#  exp(v3)*df$choice3  
+  p  <- (exp(v1)*df$choice1 + exp(v2) * df$choice2  + exp(v4) * df$choice4 + exp(v3) * df$choice3
+         + exp(v5) * df$choice5) / (exp(v1) + exp(v2)+ exp(v3) + exp(v4) + exp(v5))
+  
+  return(p)
+}
+
+set.seed(2017)
+
+doHB(likelihood, df, control)
+
+
+#---------------Sample-level main effect coefficient means----------------------
+
+model <- doHB(likelihood, df, control)
+
+dfA <- read_csv("hb_likelihood_A.csv")
+
+dfA_select <- dfA %>% select(2:8)
+
+sapply(dfA_select, function(dfA_select) c( 
+    "Stand dev" = sd(dfA_select), 
+    "Mean"= mean(dfA_select,na.rm=TRUE),
+    "n" = length(dfA_select),
+    "Median" = median(dfA_select),
+    "CoeffofVariation" = sd(dfA_select)/mean(dfA_select,na.rm=TRUE),
+    "Minimum" = min(dfA_select),
+    "Maximun" = max(dfA_select),
+    "Upper Quantile" = quantile(dfA_select,1),
+    "LowerQuartile" = quantile(dfA_select,0)
+  )
+)
+
+
+
+# B Average individual level draws 
+head(model[["B"]]) # not use in this project
+
+
+# end 
+
+
+
+
+
+
+
